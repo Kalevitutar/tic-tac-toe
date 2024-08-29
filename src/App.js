45443d5807acec400d14470597b5e2f79829bd3f
Kalevitutar <tanyamthomas@yahoo.com>
2024-08-29 14:39:10 -0400

    breaking into components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
-  return (<button className="square" onClick={handleClick}>{value}</button>);
+import Board from "./Components/Board";
+
+export default function App() {
+  return(<Board/>)
 }
 
-console.log("adding this in to test whether Github problem is resolved or not - testing again");
+// function Square() {
+//   const [value, setValue] = useState(null);
+//   function handleClick() {
+//     setValue("X");
+//   }
+//   return (<button className="square" onClick={handleClick}>{value}</button>);
+// }
 
-export default function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
-}
+
