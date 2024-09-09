ef2489e144c03c54c9a4932feef32524297cace3
Kalevitutar <tanyamthomas@yahoo.com>
2024-09-09 17:24:55 -0400

    finishing up the main tutorial, did the extra with the ability to skip back steps, and done some css to make it look purplier and all-around lovely

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
+import Game from "./Components/Game";
 
-import Board from "./Components/Board";
-
-export default function App() {
-  return(<Board/>)
+function App() {
+  
+  return(<Game/>)
+  
 }
 
-// function Square() {
-//   const [value, setValue] = useState(null);
-//   function handleClick() {
-//     setValue("X");
-//   }
-//   return (<button className="square" onClick={handleClick}>{value}</button>);
-// }
-
-
+export default App;
